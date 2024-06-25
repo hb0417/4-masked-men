9db071857bf87a6f395d88be14c7d8f6f5572541
hb0417 <136002080+hb0417@users.noreply.github.com>
2024-06-25 16:46:05 +0900

    인트로 수정후

--- Assets/Script_HB/MainMenu.cs
+++ Assets/Script_HB/MainMenu.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 
-//여현빈
-// UI관련
+
+// 메인씬 UI관련
 public class MainMenu : MonoBehaviour
 {
-    public string collectionSceneName; // 유니티 에디터에서 설정할 씬 이름 입력
+    // public string collectionSceneName; // 유니티 에디터에서 설정할 씬 이름 입력
 
     // Start is called before the first frame update
     void Start()
@@ -40,7 +40,7 @@ public class MainMenu : MonoBehaviour
     public void OnClickCollection()
     {
         Debug.Log("겔러리");
-        SceneManager.LoadScene(collectionSceneName); // 설정한 씬 이름으로 이동
+        // SceneManager.LoadScene(collectionSceneName); // 설정한 씬 이름으로 이동
     }
 
     public void OnClickSetting()
